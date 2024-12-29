4e95c48915ad4abd108facc9d346a240c721c017
deepbuzin <contactbuzin@gmail.com>
2024-12-29 15:29:03 +0700

    Implement UI layout

--- lua/gel-query.lua
+++ lua/gel-query.lua
@@ -20,6 +20,86 @@ local get_selection = function()
     print(table.concat(query, "\n"))
 end
 
-vim.keymap.set("v", "<leader>eq", get_selection)
+local execute_query = function(query)
+    -- Run a query against local Gel project
+end
+
+local open_float = function(config, enter)
+    if enter == nil then
+        enter = false
+    end
+
+    local buf = vim.api.nvim_create_buf(false, true)
+    local win = vim.api.nvim_open_win(buf, enter, config)
+
+    return { buf = buf, win = win }
+end
+
+local create_ui = function()
+    local ui_height = math.floor(vim.o.lines * 0.8)
+    local ui_width = math.floor(vim.o.columns * 0.8)
+    local top_row = math.floor((vim.o.lines - ui_height) / 2)
+    local left_col = math.floor((vim.o.columns - ui_width) / 2)
+
+    ---@type vim.api.keyset.win_config[]
+    local configs = {
+        query = {
+            relative = "editor",
+            height = math.floor(ui_height / 2) - 1, -- offset for border
+            width = math.floor(ui_width / 2) - 1,
+            row = top_row,
+            col = left_col,
+            style = "minimal",
+            border = "rounded",
+            title = "Query",
+            title_pos = "center"
+        },
+        params = {
+            relative = "editor",
+            height = math.floor(ui_height / 2) - 1, -- offset for border
+            width = math.floor(ui_width / 2) - 1,
+            row = math.floor(top_row + ui_height / 2) + 1,
+            col = left_col,
+            style = "minimal",
+            border = "rounded",
+            title = "Params",
+            title_pos = "center",
+        },
+        output = {
+            relative = "editor",
+            height = ui_height,
+            width = math.floor(ui_width / 2) - 1,
+            row = top_row,
+            col = math.floor(left_col + ui_width / 2) + 1,
+            style = "minimal",
+            border = "rounded",
+            title = "Output",
+            title_pos = "center",
+        }
+    }
+
+    local query_float = open_float(configs.query)
+    local params_float = open_float(configs.params, true)
+    local output_float = open_float(configs.output)
+
+    vim.api.nvim_create_autocmd("BufLeave", {
+        buffer = params_float.buf,
+        callback = function()
+            pcall(vim.api.nvim_win_close, query_float.win, true)
+            pcall(vim.api.nvim_win_close, output_float.win, true)
+
+            pcall(vim.api.nvim_buf_delete, query_float.buf, { force = true })
+            pcall(vim.api.nvim_buf_delete, params_float.buf, { force = true })
+            pcall(vim.api.nvim_buf_delete, output_float.buf, { force = true })
+        end
+    })
+end
+
+-- vim.keymap.set("v", "<leader>eq", get_selection)
+vim.keymap.set("v", "<leader>eee", create_ui)
+
+
+create_ui()
+
 
 return M
